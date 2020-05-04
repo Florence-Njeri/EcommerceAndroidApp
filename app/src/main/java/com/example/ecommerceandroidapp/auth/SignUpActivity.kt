c52d936b8accdd674c9a57e969cbd4d4070a31fb
Florence-Njeri <flonsie17@gmail.com>
2020-05-04 15:56:32 +0300

    add google Sign Up code

--- app/src/main/java/com/example/ecommerceandroidapp/auth/SignUpActivity.kt
+++ app/src/main/java/com/example/ecommerceandroidapp/auth/SignUpActivity.kt
@@ -1,5 +1,7 @@
 package com.example.ecommerceandroidapp.auth
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -11,12 +13,16 @@ import androidx.lifecycle.ViewModelProviders
 import com.example.ecommerceandroidapp.MainActivity
 import com.example.ecommerceandroidapp.R
 import com.example.ecommerceandroidapp.databinding.ActivitySignUpBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
+
 
 class SignUpActivity : AppCompatActivity(), AuthListener {
     private lateinit var binding: ActivitySignUpBinding
     private lateinit var auth: FirebaseAuth
     private lateinit var viewModel: AuthViewModel
+    private val RC_SIGN_IN = 1822
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel=ViewModelProviders.of(this).get(AuthViewModel::class.java)
@@ -32,12 +38,49 @@ class SignUpActivity : AppCompatActivity(), AuthListener {
             startActivity(Intent(this,LogInActivity::class.java))
 
         }
+
+        binding.googleSignInButton.setOnClickListener {
+            signInWithGoogle()
+        }
     }
 
+    private fun signInWithGoogle() {
+
+        val providers = listOf(AuthUI.IdpConfig.GoogleBuilder().build())
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN
+        )
+    }
+
+    @SuppressLint("TimberArgCount")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val response: IdpResponse? = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                Toast.makeText(this@SignUpActivity, "Successfully signed up", Toast.LENGTH_SHORT)
+                    .show()
+
+                startActivity(Intent(this@SignUpActivity, MainActivity::class.java))
+            } else {
+                Toast.makeText(
+                    this@SignUpActivity,
+                    "Unsuccessful ${response?.error?.errorCode}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
 
     override fun onStarted() {
         binding.progressBar.visibility = View.VISIBLE
     }
+
 
     override fun onSuccess() {
         binding.progressBar.visibility = View.GONE
