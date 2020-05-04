977514d81910ef51964f4123ccc376d22044618e
Florence-Njeri <flonsie17@gmail.com>
2020-05-04 16:01:48 +0300

    add google Log In code

--- app/src/main/java/com/example/ecommerceandroidapp/auth/LogInActivity.kt
+++ app/src/main/java/com/example/ecommerceandroidapp/auth/LogInActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ecommerceandroidapp.auth
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -11,12 +12,16 @@ import androidx.lifecycle.ViewModelProviders
 import com.example.ecommerceandroidapp.MainActivity
 import com.example.ecommerceandroidapp.R
 import com.example.ecommerceandroidapp.databinding.ActivityLogInBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class LogInActivity : AppCompatActivity(), AuthListener {
     private lateinit var binding: ActivityLogInBinding
     private lateinit var auth: FirebaseAuth
     private lateinit var viewModel: AuthViewModel
+    private val RC_SIGN_IN: Int = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(AuthViewModel::class.java)
@@ -32,6 +37,45 @@ class LogInActivity : AppCompatActivity(), AuthListener {
 
             //Navigate to forgot password screen
 
+        }
+
+        googleSignInButton.setOnClickListener {
+            signInWithGoogle()
+        }
+    }
+
+    private fun signInWithGoogle() {
+        val providers = listOf(AuthUI.IdpConfig.GoogleBuilder().build())
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+
+                val currentUser = FirebaseAuth.getInstance().currentUser
+                if (currentUser == null) {
+                    // User logging in after log out, check if there are shared prefs
+
+                    startActivity(Intent(this@LogInActivity, MainActivity::class.java))
+                } else {
+                    //                    Log in to the main Activity
+                    startActivity(Intent(this@LogInActivity, MainActivity::class.java))
+                }
+
+            } else {
+                Toast.makeText(this@LogInActivity, "Please try again", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
